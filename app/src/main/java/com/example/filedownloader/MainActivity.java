eb8bd27cc59ef2ef627100db3fbb7e0d8b1f8371
Riley Larget <larget@wisc.edu>
2021-10-08 11:26:04 -0500

    M1-3

--- app/src/main/java/com/example/filedownloader/MainActivity.java
+++ app/src/main/java/com/example/filedownloader/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.filedownloader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mockFileDownloader() {
+        for (int downloadProgress=0; downloadProgress <= 100; downloadProgress += 10) {
+            Log.d(TAG,"Download Progress: "+downloadProgress+"%");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+    }
+
+    public void startDownload(View view) {
+        mockFileDownloader();
+    }
+
+    public void stopDownload(View view) {
     }
 }
